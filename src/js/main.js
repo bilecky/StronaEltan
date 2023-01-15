d66cbf5550ae5a33bf1ea0caa6cd1ebd29c3ac66
bilecky <bilecky95@gmail.com>
2023-01-15 20:36:48 +0100

    init app

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.btn')
+const menu = document.querySelector('.menu')
+const allNavItems = document.querySelectorAll('.menu__item')
+
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('is-active')
+    btn.classList.toggle('is-active')
+
+    allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			menu.classList.remove('is-active')
+            btn.classList.remove('is-active')
+
+
+		})
+	})
+})
