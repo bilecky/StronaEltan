3ce64a76b61bf0581fcd76460780acdf47b3f895
bilecky <bilecky95@gmail.com>
2023-01-16 00:37:25 +0100

    added alert

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,18 @@ const btn = document.querySelector('.btn')
 const menu = document.querySelector('.menu')
 const allNavItems = document.querySelectorAll('.menu__item')
 
-
 btn.addEventListener('click', () => {
-    menu.classList.toggle('is-active')
-    btn.classList.toggle('is-active')
+	menu.classList.toggle('is-active')
+	btn.classList.toggle('is-active')
 
-    allNavItems.forEach(item => {
+	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			menu.classList.remove('is-active')
-            btn.classList.remove('is-active')
-
-
+			btn.classList.remove('is-active')
 		})
 	})
 })
 
+function onSubmit() {
+	alert('Twoja wiadomość została wysłana!')
+}
